7cd8045f40754373c495951145e7434c78444b30
SHIKHAR458 <shikharp108@gmail.com>
2025-12-05 12:33:30 +0000

    variables_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 9980
+let accountEmail = " shikharpatel@gmail.com"
+var accountPassword = "8r909"
+
+
+// accountId = 8   -> not allowed  
+accountEmail = "okbye"
+accountPassword = "09009"
+
+console.log(accountId)
+
+
+console.table([accountEmail,accountPassword])

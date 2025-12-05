84d6317ac703e71ffb228bb0f077b763f331ac2f
SHIKHAR458 <shikharp108@gmail.com>
2025-12-05 13:52:13 +0000

    typeconversion

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,15 @@
+let score = 33
+
+let age = "78"
+let newAge = "98ol"
+
+console.log(typeof score)
+console.log(typeof age)
+
+let stringToNumber = Number(score)
+let newNum = Number(newAge)
+
+console.log(stringToNumber)
+console.log(typeof stringToNumber)
+console.log(typeof newAge) // par ye galat hai
+console.log(newNum)

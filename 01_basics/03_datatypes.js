e2c16671aeee4cf051344cd2943c1bb08977a90a
SHIKHAR458 <shikharp108@gmail.com>
2025-12-06 08:23:38 +0000

    conversions

--- 01_basics/03_datatypes.js
+++ 01_basics/03_datatypes.js
@@ -3,19 +3,20 @@ let score = 33
 let age = "78"
 let newAge = "98ol"
 
-console.log(typeof score)
-console.log(typeof age)
+//console.log(typeof score)
+//console.log(typeof age)
 
 let stringToNumber = Number(score)
 let newNum = Number(newAge)
 
-console.log(stringToNumber)
-console.log(typeof stringToNumber)
-console.log(typeof newAge) // par ye galat hai
-console.log(newNum)
+//console.log(stringToNumber)
+//console.log(typeof stringToNumber)
+//console.log(typeof newAge) // par ye galat hai
+//console.log(newNum)
 // 1 -> true, 0 -> false
 
 let islooged = 1
 console.log(typeof islooged)
 
-let ooged = Boolean
+let looged = Boolean(islooged)
+console.log(typeof looged)

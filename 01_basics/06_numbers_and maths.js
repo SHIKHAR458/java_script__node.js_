6c1b206090fcca1cbd7a2b7cb25be2ffa0646f04
SHIKHAR458 <shikharp108@gmail.com>
2025-12-07 12:22:59 +0000

    maths

---
+++ 01_basics/06_numbers_and maths.js
@@ -0,0 +1,26 @@
+const number = 100
+
+const score = new Number(100)
+
+console.log(score)
+console.log(number)
+
+console.log(score.toString().length)
+console.log(score.toFixed(3))
+
+const withDecimal = 1209.94623
+
+console.log(withDecimal.toPrecision(5))
+console.log(withDecimal.toFixed(2))
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-IN'))
+
+//---------------maths-------------//
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.random())

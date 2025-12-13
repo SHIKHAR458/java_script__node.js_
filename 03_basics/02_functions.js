7009c2c7808f21b598f2b4bcc302ad08bc28899d
SHIKHAR458 <shikharp108@gmail.com>
2025-12-13 11:00:51 +0000

    funtion_02

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,28 @@
+function calculateCartPrice(...num1){
+    return num1
+}
+//    yaha pe ... ye ek rest operator hai 
+
+console.log(calculateCartPrice(200,500,700,500))
+
+
+// now agar aisa ho ki 
+
+function boplll(val1 , val2 , ...num1){
+    return num1
+}
+
+console.log(boplll(200 ,90 ,98,78,65))
+
+
+///handling objects 
+const user = {
+    username : "hitesh",
+    prices : 900
+}
+
+function handlingobjects(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.prices}`)
+}
+
+handlingobjects(user)

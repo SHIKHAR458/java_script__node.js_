40b6dd281cb8cd7741f22e2556a657f24e85cffb
SHIKHAR458 <shikharp108@gmail.com>
2025-12-29 10:52:34 +0000

    dom

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,12 @@
-let heading = document.getElementById("heading")
-console.log(heading)
+//let heading = document.getElementById("heading")
+//console.log(heading)
+
+//let headings = document.getElementsByClassName("heading2")
+//console.dir(headings)
+//console.log(headings)
+
+//let parahs = document.getElementsByTagName("p")
+//console.dir(parahs)
+
+let elements = document.querySelector("p")
+console.dir(elements)

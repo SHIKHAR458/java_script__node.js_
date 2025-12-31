e37a5049fa09118702a4460412b9c2ea2cc85107
SHIKHAR458 <shikharp108@gmail.com>
2025-12-31 19:08:35 +0000

    doms

--- DOM/script.js
+++ DOM/script.js
@@ -8,5 +8,18 @@
 //let parahs = document.getElementsByTagName("p")
 //console.dir(parahs)
 
-let elements = document.querySelector("p")
-console.dir(elements)
+//let elements = document.querySelector("p")
+//console.dir(elements)
+
+let btl1 = document.querySelector("#btl1")
+btl1.onclick = () => {
+    console.log("btl1 was clicked")
+    let a = 25;
+    a++;
+    console.log(a)
+}
+
+let div = document.querySelector("div")
+div.onmouseover  = () => {
+    console.log("you are inside div")
+}
